976c0ce2eabbfda647cfd7f07bd60cc2c4e46b2c
Davidd7 <37209693+Davidd7@users.noreply.github.com>
2022-12-27 17:47:20 +0100

    (Playing with file upload functionality)

--- my_package/client/form_elements/Input3DModel.svelte
+++ my_package/client/form_elements/Input3DModel.svelte
@@ -26,41 +26,35 @@
 		inputText.value = "";
 		inputText.placeholder = "Loading...";
 
-		let success = false;
-		let attempts = 0;
+		try {
 
-		while (!success && attempts < 1) {
-
-			try {
-
-				let response = await fetch('/files', {
-					method: 'POST',
-					body: formData
-				});
-				
-				if (!response.ok) {
-					throw new Error('Something went wrong');
-					// inputText.placeholder = "Error: " + error;
-				}
-				let response_data = await response.text();
-
-				if (response_data == "") {
-					throw new Error('Something went wrong');
-				}
-				data[valueKey] = response_data;
-				inputText.placeholder = "No file uploaded";
-				success = true;
-				console.log("Upload was successful")
-
-			} catch (error) {
-				inputText.placeholder = "Error: " + error;
-				console.log("Attempt " + (attempts + 1) + " of 1 unsuccessful.");
-				console.log("If you are running flask on the dev server, use a production server instead for successfull file uploads.");
-				attempts += 1;
-				await sleep(1000);
+			let response = await fetch('/files', {
+				method: 'POST',
+				body: formData
+			});
+			
+			if (!response.ok) {
+				throw new Error('Something went wrong');
 			}
+			let response_data = await response.text();
 
+			if (response_data == "") {
+				throw new Error('Something went wrong');
+			}
+			
+			data[valueKey] = response_data;
+			inputText.placeholder = "No file uploaded";
+			inputFile.value = "";
+			console.log("Upload was successful")
+
+		} catch (error) {
+			inputText.placeholder = "Error. Please try again.";
+			inputFile.value = "";
+			console.log("Attempt unsuccessful.");
+			console.log("If you are running flask on the dev server, use a production server instead for successfull file uploads.");
+			await sleep(1000);
 		}
+
 
 
 
